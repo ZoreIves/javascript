c01c8823e8529fd875075d8de70698ec33dcfecb
Catur Hidayat <c.meong@gmail.com>
2020-10-23 22:01:53 +0700

    perbedaan variable

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ let msg1 = "Hallo";
 
 const msg2 = "Javascript"
 
+if(true){
+    var message = "Javascript";
+    let msg1 = 2;
+    console.log("testing", msg1);
+}
 
 console.log(message)
+console.log(msg1)
