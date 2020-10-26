409544af817c1206753ede273a933e55c889c5ef
Catur Hidayat <c.meong@gmail.com>
2020-10-26 19:47:28 +0700

    function

---
+++ function.js
@@ -0,0 +1,17 @@
+function jalanSatu(){
+    console.log("Satu");
+}
+
+const jalanDua = function(){
+    console.log("Dua");
+}
+
+jalanSatu()
+jalanDua()
+
+
+const jalanTiga = ()=>{
+    console.log("tiga");
+}
+
+jalanTiga()
