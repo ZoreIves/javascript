1a5ab13fdf7fc56d72b4767c913f6db11ebb9f42
Catur Hidayat <c.meong@gmail.com>
2020-10-29 12:50:22 +0700

    if else

---
+++ if-else-if.js
@@ -0,0 +1,11 @@
+var a = 31
+
+if(a>50){
+    console.log(" Lebih Besar dari 50 ");
+}else if(a>40){
+    console.log("Lebih Besar dari 40 ");
+}else if(a<30 || a>0){
+    console.log("Kurang dari 30");
+}else{
+    console.log("Tidak diketahui");
+}
---
+++ if-else.js
@@ -0,0 +1,12 @@
+function kondisi(){
+    var nilai = 15
+    if(nilai === 10){
+        console.log("Terbilang sepuluh");
+    }else if(nilai === 15){
+        console.log("Terbilang Lima Belas");
+    }else{
+        console.log("Tidak ada yang benar");
+    }
+}
+
+kondisi()
