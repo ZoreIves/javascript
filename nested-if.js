47a174398bd412d299de06c99310228a8e71bbf6
Catur Hidayat <c.meong@gmail.com>
2020-10-29 16:34:29 +0700

    nested if

---
+++ nested-if.js
@@ -0,0 +1,18 @@
+var x = 830
+var y = 620
+var z = 580
+
+
+if(x>y){
+    if(x>z){
+        console.log("X nilai terbesar");
+    }else{
+        console.log("X nilai terbesar kedua");
+    }
+}else{
+    if(x<y){
+        console.log("X nilai terkecil");
+    }else{
+        console.log("X nilai Terkecil kedua");
+    }
+}
