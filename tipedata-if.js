87d542fbb212ccb3e5c93ee9955f27790ee8b891
Catur Hidayat <c.meong@gmail.com>
2020-10-30 22:19:43 +0700

    if tipedata(2)

---
+++ tipedata-if.js
@@ -0,0 +1,13 @@
+function tipeData(){
+    var data = 329
+    
+    console.log(typeof(data));
+
+    if(data == "329"){
+        return data
+    }else{
+        return "Bukan"
+    }
+}
+
+console.log(tipeData());
