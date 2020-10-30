71926b36e337400609e6a5b97e6842be5f84ef14
Catur Hidayat <c.meong@gmail.com>
2020-10-30 22:43:03 +0700

    switch case

---
+++ switch.js
@@ -0,0 +1,29 @@
+
+function Nilai(){
+    var nilaiMatematika = 90;
+
+    switch(nilaiMatematika){
+        case 100:
+            console.log("Sempurna");
+            break;
+    
+        case 90:
+            console.log("Sangat Bagus");
+            break;
+    
+        case 80:
+            console.log("Bagus");
+            break;
+    
+        case 70:
+            console.log("Cukup Bagus");
+            break;
+    
+        default:
+            console.log("Kurang");
+    }
+
+}
+
+console.log(Nilai());
+
