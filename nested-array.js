f0d298def440206a54abf0ef60aae6bf1a349fbe
Catur Hidayat <c.meong@gmail.com>
2020-11-15 19:08:45 +0700

    nested array

---
+++ nested-array.js
@@ -0,0 +1,10 @@
+function nestedArray(){
+    namaArray = [["satu", "Dua", "Tiga"], 
+                 ["Code1", "Code2", "Code3"], 
+                 ["Javascript", "CSS", "HTML"]]
+
+    console.log(namaArray.length)
+    console.log(namaArray[1][1])
+}
+
+nestedArray();
