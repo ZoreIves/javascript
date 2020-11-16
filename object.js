dc480662200846e3bbd4fe3d0daed3c42cfaedab
Catur Hidayat <c.meong@gmail.com>
2020-11-16 21:05:55 +0700

    array di dalam object

--- object.js
+++ object.js
@@ -2,9 +2,12 @@ function pemainBola(){
     var pemain = {
         nama : "Ronaldo",
         tinggi : "190",
-        club : "Juve"
+        club : "Juve",
+        tahun : [2017, 2018, 2019, 2020]
     }
-    console.log(pemain.club);
+    console.log(pemain)
+    console.log(pemain.club)
+    console.log(pemain.tahun[3])
 }
 
 pemainBola();
