1a51df5efce8e4d194f23194bea5cec38ead6e37
Catur Hidayat <c.meong@gmail.com>
2020-11-16 21:42:23 +0700

    property

---
+++ property.js
@@ -0,0 +1,15 @@
+function pemainBola(){
+    var pemain = {
+        nama : "Ronaldo",
+        tinggi : "190",
+        club : "Juve",
+        tahun : [2017, 2018, 2019, 2020]
+    }
+
+    pemain.tinggi = 185
+    console.log(pemain)
+    console.log(pemain.club)
+    console.log(pemain.tahun[3])
+}
+
+pemainBola();
