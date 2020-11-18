ab3513079567ed99c807062f617f79c7a5624c8d
Catur Hidayat <c.meong@gmail.com>
2020-11-18 21:22:30 +0700

    nested object

---
+++ nestedObject.js
@@ -0,0 +1,16 @@
+function nestedFunction(){
+    var coding = {
+        kelas : "coding",
+        materi : {
+            materi1 : "HTML",
+            materi2 : "CSS",
+            materi3 : "Javascript",
+            materi4 : "NodeJS",
+            materi5 : "MongoDB"
+        }
+
+    }
+    console.log(coding.materi.materi4);
+}
+
+nestedFunction()
