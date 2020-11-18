31ee6d43d7660d14d48bbe4e83587a7063f4a07b
Catur Hidayat <c.meong@gmail.com>
2020-11-18 21:14:41 +0700

    function object

---
+++ functionObject.js
@@ -0,0 +1,13 @@
+function fungsiObject(){
+
+    var a = {
+        salam : belajarJavascript()
+    }
+    console.log(a.salam);
+}
+
+function belajarJavascript(){
+    return "Sedang belajar javascript dasar brohh!!!"
+}
+
+fungsiObject()
