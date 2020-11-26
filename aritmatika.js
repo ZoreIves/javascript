861f04c85c332af04976f9253211ccb8b26a5a78
Catur Hidayat <c.meong@gmail.com>
2020-11-26 20:33:22 +0700

    Operator aritmatika

---
+++ aritmatika.js
@@ -0,0 +1,8 @@
+var a = 35;
+var b = 40;
+
+console.log(a + b); // Penjumlahan
+console.log(a - b); // Pengurangan
+console.log(a * b); // Perkalian
+console.log(a / b); // Pembagian
+console.log(a % b); // Sisa pembagian
