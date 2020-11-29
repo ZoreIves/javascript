30a74ad82684391dbcef293b853675942df306d7
Catur Hidayat <c.meong@gmail.com>
2020-11-29 14:50:26 +0700

    Perbandingan operator

---
+++ perbandingan.js
@@ -0,0 +1,11 @@
+var a = 33;
+var b = 35;
+
+console.log(a>b);   // Lebih dari
+console.log(a<b);   // Kurang dari
+console.log(a>=b);  // Lebih dari sama dengan
+console.log(a<=b);  // Kurang dari sama dengan
+console.log(a==b);  // Sama Dengan
+console.log(a===b); // Sama dengan spesifik type data
+console.log(a!=b);  // tidak sama dengan
+console.log(a!==b); // tidak sama dengan spesifik type data
