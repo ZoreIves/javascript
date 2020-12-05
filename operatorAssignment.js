d82bb87d373a876e905a24f981d4a0b7cc09e840
Catur Hidayat <c.meong@gmail.com>
2020-12-05 16:01:38 +0700

    fungsi spread, untuk menggabungkan array

---
+++ operatorAssignment.js
@@ -0,0 +1,9 @@
+var a;
+var b;
+var c;
+
+a = "Belajar "
+b = "Javascript "
+c = "Pemula"
+
+console.log(a,b,c);
---
+++ spread.js
@@ -0,0 +1,8 @@
+var num1 = [12,13,14,15,16]
+
+var num2 = [21,22,23,24,25]
+
+var sprd = [...num1,...num2,103,105]
+
+console.log(num1, num2);
+console.log(sprd);
